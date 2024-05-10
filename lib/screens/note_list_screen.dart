a93d979059e31835e2d36c47d47287e0675c16ef
kristfer <kristfernando8@gmail.com>
2024-05-10 09:41:47 +0700

    10/05/2024 Update

--- lib/screens/note_list_screen.dart
+++ lib/screens/note_list_screen.dart
@@ -63,6 +63,14 @@ class NoteList extends StatelessWidget {
                         },
                       );
                     },
+                    leading: document.imageUrl != null
+                        ? CircleAvatar(
+                            backgroundImage: NetworkImage(document.imageUrl!),
+                          )
+                        : const CircleAvatar(
+                            backgroundColor: Colors.grey,
+                            child: Icon(Icons.image),
+                          ),
                     title: Text(document.title),
                     subtitle: Text(document.description),
                     trailing: InkWell(
--- lib/widgets/note_dialog.dart
+++ lib/widgets/note_dialog.dart
@@ -1,23 +1,48 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:notes/models/note.dart';
 import 'package:notes/services/note_service.dart';
 
-class NoteDialog extends StatelessWidget {
+class NoteDialog extends StatefulWidget {
   final Note? note;
+
+  NoteDialog({super.key, this.note});
+
+  @override
+  State<NoteDialog> createState() => _NoteDialogState();
+}
+
+class _NoteDialogState extends State<NoteDialog> {
   final TextEditingController _titleController = TextEditingController();
   final TextEditingController _descriptionController = TextEditingController();
+  File? _imageFile;
 
-  NoteDialog({super.key, this.note}) {
-    if (note != null) {
-      _titleController.text = note!.title;
-      _descriptionController.text = note!.description;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    if (widget.note != null) {
+      _titleController.text = widget.note!.title;
+      _descriptionController.text = widget.note!.description;
+    }
+  }
+
+  Future<void> _pickImage() async {
+    final PickedFile =
+        await ImagePicker().pickImage(source: ImageSource.gallery);
+    if (PickedFile != null) {
+      setState(() {
+        _imageFile = File(PickedFile.path);
+      });
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
-      title: Text(note == null ? 'Add Notes' : 'Update Notes'),
+      title: Text(widget.note == null ? 'Add Notes' : 'Update Notes'),
       content: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -37,6 +62,17 @@ class NoteDialog extends StatelessWidget {
           TextField(
             controller: _descriptionController,
           ),
+          const Padding(
+            padding: EdgeInsets.only(top: 20),
+            child: Text(
+              'Image : ',
+            ),
+          ),
+          _imageFile != null ? Image.file(_imageFile!) : Container(),
+          TextButton(
+            onPressed: _pickImage,
+            child: const Text('Pick Image'),
+          )
         ],
       ),
       actions: [
@@ -50,8 +86,20 @@ class NoteDialog extends StatelessWidget {
           ),
         ),
         ElevatedButton(
-          onPressed: () {
-            if (note == null) {
+          onPressed: () async {
+            String? imageUrl;
+            if (_imageFile != null) {
+              imageUrl = await NoteService.uploadImage(_imageFile!);
+            }
+            Note note = Note(
+              id: widget.note?.id,
+              title: _titleController.text,
+              description: _descriptionController.text,
+              imageUrl: imageUrl,
+              createdAt: widget.note?.createdAt,
+            );
+
+            if (widget.note == null) {
               NoteService.addNote(Note(
                 title: _titleController.text,
                 description: _descriptionController.text,
@@ -60,14 +108,14 @@ class NoteDialog extends StatelessWidget {
               });
             } else {
               NoteService.updateNote(Note(
-                id: note!.id,
+                id: widget.note!.id,
                 title: _titleController.text,
                 description: _descriptionController.text,
-                createdAt: note!.createdAt,
+                createdAt: widget.note!.createdAt,
               )).whenComplete(() => Navigator.of(context).pop());
             }
           },
-          child: Text(note == null ? 'Add' : 'Update'),
+          child: Text(widget.note == null ? 'Add' : 'Update'),
         ),
       ],
     );
